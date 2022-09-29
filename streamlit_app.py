b9e63984d1eaba0563a358a7d10d464f286f529c
Arnaud 🎈 <arnaudmiribel@gmail.com>
2022-09-30 00:38:55 +0200

    Add desc.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,60 +9,10 @@ from streamlit_faker import (
     text_commands,
 )
 
-# with st.echo():
-#     from streamlit_faker import StreamlitFaker as stfk
-
-#     stfk.title()
-#     stfk.markdown()
-#     stfk.caption()
-#     stfk.text_input()
-#     stfk.text_input()
-#     stfk.text_area()
-#     stfk.subheader(body="You can also choose!")
-#     stfk.line_chart()
-#     stfk.subheader()
-#     stfk.altair_chart()
-#     left, right = st.columns(2)
-#     with left:
-#         stfk.caption()
-#         stfk.bar_chart()
-#     with right:
-#         stfk.caption()
-#         stfk.bar_chart()
-#     stfk.markdown()
-#     stfk.text()
-
-#     left, middle, right = st.columns(3)
-#     with left:
-#         stfk.metric()
-#     with middle:
-#         stfk.metric()
-#     with right:
-#         stfk.metric()
-
-#     stfk.code()
-#     stfk.latex()
-#     stfk.json()
-#     stfk.button()
-#     stfk.checkbox()
-#     stfk.radio()
-#     stfk.selectbox()
-#     stfk.multiselect()
-#     stfk.slider()
-#     stfk.color_picker()
-#     # stfk.camera_input()
-#     stfk.number_input()
-#     stfk.date_input()
-#     stfk.error()
-#     stfk.success()
-#     stfk.info()
-#     stfk.balloons()
-#     stfk.warning()
-
 st.title("🥷")
 st.title("Streamlit Faker")
 st.subheader("Description")
-st.text("(WIP)")
+st.text("This library offers a way to quickly fake some Streamlit commands.")
 st.subheader("Supported commands")
 for cmd in (
     text_commands

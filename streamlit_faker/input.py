4f5af5525ae855fdcc7cf76def6a391fa406fdce
Arnaud 🎈 <arnaudmiribel@gmail.com>
2022-09-30 00:05:44 +0200

    Add inputs

--- streamlit_faker/input.py
+++ streamlit_faker/input.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from faker import Faker
-from faker.providers import BaseProvider
+from faker.providers import BaseProvider, date_time
 
 from .common import st_command_with_default
 
 fake = Faker()
+fake.add_provider(date_time)
 
 
 class StreamlitInputsProvider(BaseProvider):
@@ -44,28 +45,88 @@ class StreamlitInputsProvider(BaseProvider):
         raise NotImplementedError
 
     def checkbox(self, **kwargs):
-        raise NotImplementedError
+        return st_command_with_default(
+            st.checkbox,
+            {
+                "label": fake.sentence(),
+                "value": self.random_element((True, False)),
+                "key": fake.name().lower(),
+            },
+            **kwargs,
+        )
 
     def radio(self, **kwargs):
-        raise NotImplementedError
+        return st_command_with_default(
+            st.radio,
+            {
+                "label": fake.name(),
+                "options": fake.sentence().lower().split(),
+                "index": 0,
+                "key": fake.name().lower(),
+            },
+        )
 
     def selectbox(self, **kwargs):
-        raise NotImplementedError
+        return st_command_with_default(
+            st.selectbox,
+            {
+                "label": fake.name(),
+                "options": fake.sentence().lower().split(),
+                "index": 0,
+                "key": fake.name().lower(),
+            },
+        )
 
     def multiselect(self, **kwargs):
-        raise NotImplementedError
+        options = fake.sentence().lower().split()
+        return st_command_with_default(
+            st.multiselect,
+            {
+                "label": fake.name(),
+                "options": options,
+                "default": options[0],
+                "key": fake.name().lower(),
+            },
+            **kwargs,
+        )
 
     def slider(self, **kwargs):
-        raise NotImplementedError
+        return st_command_with_default(
+            st.slider,
+            {
+                "label": fake.name(),
+                "min_value": self.random_int(0, 10),
+                "max_value": self.random_int(50, 100),
+                "key": fake.name().lower(),
+            },
+            **kwargs,
+        )
 
     def select_slider(self, **kwargs):
         raise NotImplementedError
 
     def number_input(self, **kwargs):
-        raise NotImplementedError
+        return st_command_with_default(
+            st.number_input,
+            {
+                "label": fake.name(),
+                "min_value": self.random_int(0, 10),
+                "max_value": self.random_int(50, 100),
+                "key": fake.name().lower(),
+            },
+            **kwargs,
+        )
 
     def date_input(self, **kwargs):
-        raise NotImplementedError
+        return st_command_with_default(
+            st.date_input,
+            {
+                "label": fake.name(),
+                "value": fake.date_this_year(),
+                "key": fake.name().lower(),
+            },
+            **kwargs,
+        )
 
     def time_input(self, **kwargs):
         raise NotImplementedError
@@ -74,7 +135,21 @@ class StreamlitInputsProvider(BaseProvider):
         raise NotImplementedError
 
     def camera_input(self, **kwargs):
-        raise NotImplementedError
+        return st_command_with_default(
+            st.camera_input,
+            {
+                "label": fake.name(),
+                "key": fake.name().lower(),
+            },
+            **kwargs,
+        )
 
     def color_picker(self, **kwargs):
-        raise NotImplementedError
+        return st_command_with_default(
+            st.color_picker,
+            {
+                "label": fake.name(),
+                "key": fake.name().lower(),
+            },
+            **kwargs,
+        )
